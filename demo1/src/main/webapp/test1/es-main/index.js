b1aef83a63e8e4504c414d645a3668f82f081e58
CLovinr <zggzcyg@qq.xom>
2020-02-23 16:04:11 +0800

    完善demo1

--- demo1/src/main/webapp/test1/es-main/index.js
+++ demo1/src/main/webapp/test1/es-main/index.js
@@ -1,3 +1,4 @@
+import "css!static/test.scss";//不是以“.”开头的路径相对于baseUrl
 import Vue from "vue";
 import comp1 from "./vue/comp1.vue";//需要加入文件后缀，当前目录需要用"./"、否则是相对于baseUrl
 
--- demo1/src/main/webapp/test2/es-main/index.js
+++ demo1/src/main/webapp/test2/es-main/index.js
@@ -1,3 +1,4 @@
+import "css!static/test.scss";//不是以“.”开头的路径相对于baseUrl
 import txt from "text!./test.txt";
 
 const appDom=document.getElementById("app");
