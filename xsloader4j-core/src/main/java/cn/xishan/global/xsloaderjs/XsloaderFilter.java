d601609764344c0a90d40fead554c6690db8929b
CLovinr <zggzcyg@qq.xom>
2020-10-07 15:18:23 +0800

    修改显示版本号

--- xsloader4j-core/src/main/java/cn/xishan/global/xsloaderjs/XsloaderFilter.java
+++ xsloader4j-core/src/main/java/cn/xishan/global/xsloaderjs/XsloaderFilter.java
@@ -39,7 +39,7 @@ public class XsloaderFilter implements Filterer
 {
     private static final Logger LOGGER = LoggerFactory.getLogger(XsloaderFilter.class);
 
-    public static final String XSLOADER_VERSION="1.1.31";
+    public static final String XSLOADER_VERSION="1.2.0";
 
     private byte[] content;
     private byte[] map;
