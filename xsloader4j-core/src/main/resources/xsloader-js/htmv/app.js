e1f886e74ff5fe514a66d9f7aad112fd745505b5
CLovinr <759914852@qq.com>
2020-12-07 10:41:47 +0800

    修复htmv加载vue未完成时，导致页面渲染失败的问题

--- xsloader4j-core/src/main/resources/xsloader-js/htmv/app.js
+++ xsloader4j-core/src/main/resources/xsloader-js/htmv/app.js
@@ -1,7 +1,7 @@
 
 window.__htmv_init_bridge_=function(){
-	xsloader.define("htmv-main",["vue","default!#{app}"],function(Vue,App){
-		//this.require(["vue","default!#{app}"],function(Vue,App){
+	xsloader.define("htmv-main",["vue"],function(Vue){//须先等待vue加载完成
+		this.require(["default!#{app}"],function(App){
 			new Vue({
                 el: '##{app-id}',
                 template:"<app class='#{app-class}'></app>",
@@ -9,7 +9,6 @@ window.__htmv_init_bridge_=function(){
                     app:App
                 }
             });
-//		})
-//		.setTag("#{app}");
+		}).setTag("#{app}");
     });
 };
